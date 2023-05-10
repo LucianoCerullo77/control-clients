61b74bec01923cb16c3f3d63e0bf255587e24fce
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-09 21:12:17 -0300

    new modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TableComponent } from './components/table/table.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TableComponent,
+    ClientsComponent,
+    EditClientComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConfigurationComponent,
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

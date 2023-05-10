02189516aa51358da75e3414c1ab94fcd548f743
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-09 21:44:15 -0300

    routes added to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableComponent } from './components/table/table.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TableComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'configuration', component: ConfigurationComponent },
+  { path: 'client/edit/:id', component: EditClientComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

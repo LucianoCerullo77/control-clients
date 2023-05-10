1d90ff60e1b881283eae623dddf2fd5fc7ba30c2
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-10 08:22:28 -0300

    app component html configurated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { TableComponent } from './components/table/table.component';
 import { ClientsComponent } from './components/clients/clients.component';
 import { EditClientComponent } from './components/edit-client/edit-client.component';

eec12588c104868c22bf24a55974fa32c3b2ecb3
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-11 11:34:15 -0300

    client service created and implemented with firestore

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+  AngularFirestore,
+} from '@angular/fire/compat/firestore';
+import { Client } from '../models/client.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ClientService {
+  clientsCollection: AngularFirestoreCollection<Client>;
+  clientDoc: AngularFirestoreDocument<Client>;
+  clients: Observable<Client[]>;
+  client: Observable<Client>;
+
+  constructor(private db: AngularFirestore) {
+    this.clientsCollection = db.collection('clients', (ref) =>
+      ref.orderBy('name', 'asc')
+    );
+  }
+
+  getClients(): Observable<Client[]> {
+    this.clients = this.clientsCollection.snapshotChanges().pipe(
+      map((changes) => {
+        return changes.map((action) => {
+          const data = action.payload.doc.data() as Client;
+          data.id = action.payload.doc.id;
+          return data;
+        });
+      })
+    );
+    return this.clients;
+  }
+}

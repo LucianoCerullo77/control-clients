d7ff640d125fd778a0b4bdc71e92183217aeae9a
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-13 18:13:18 -0300

    testing form components with data added

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Client } from 'src/app/models/client.model';
 import { ClientService } from 'src/app/services/client.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-clients',
@@ -9,8 +11,17 @@ import { ClientService } from 'src/app/services/client.service';
 })
 export class ClientsComponent {
   clients: Client[];
+  client: Client = {
+    name: '',
+    lastname: '',
+    email: '',
+    balance: 0,
+  };
 
-  constructor(private clientsService: ClientService) {}
+  constructor(
+    private clientsService: ClientService,
+    private flashMessages: FlashMessagesService
+  ) {}
 
   ngOnInit() {
     this.clientsService.getClients().subscribe((clients) => {
@@ -18,13 +29,24 @@ export class ClientsComponent {
     });
   }
 
-  getTotalBalance(){
+  getTotalBalance() {
     let totalBalance: number = 0;
-    if(this.clients) {
-      this.clients.forEach(client => {
-        totalBalance += Number(client?.balance)
-      })
+    if (this.clients) {
+      this.clients.forEach((client) => {
+        totalBalance += Number(client?.balance);
+      });
     }
-    return totalBalance
+    return totalBalance;
+  }
+
+  addFormClient(formClient: NgForm) {
+    if (!formClient.valid) {
+      this.flashMessages.show('Please complete the form', {
+        cssClass: 'alert-danger',
+        timeout: 4000,
+      });
+    } else {
+      //data
+    }
   }
 }
